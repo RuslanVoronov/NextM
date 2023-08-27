42b34a4200fc7cbdab8d8b74e72375e2a6f83607
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-08-27 18:23:21 +0300

    adaprive promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -6,10 +6,10 @@ function Promo() {
 
     return (
         <main className="promo">
-            <img src={title} alt="Логотип" className="promo__title" />
-            <img src={message} alt="Логотип" className="promo__message-image" />
+            <img src={title} alt="Логотип" className="promo__title" />            
             <button className='promo__sign-up-button'>зарегистрироваться</button>
-            <h2 className='promo__subtitle'>Команда рекламного холдинга GroupM рада открыть для вас двери digital-конференции NextM!</h2>
+            <img src={message} alt="Логотип" className="promo__message-image" />
+            <h2 className='promo__subtitle'>Команда рекламного холдинга GroupM рада открыть для вас двери digital-конференции NextM!</h2>   
             <p className='promo__text'>NextM – это ежегодная digital-конференция, организованная ведущей международной рекламной группой GroupM, входящей в состав коммуникационного холдинга WPP и представленной в России агентствами Mindshare, MediaCom и Wavemaker.</p>
         </main >
     );
