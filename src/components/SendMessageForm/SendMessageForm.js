22511bad3296c9eb41ab78daef600660a6db1eee
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-08-27 17:10:55 +0300

    adaptive footer

--- src/components/SendMessageForm/SendMessageForm.js
+++ src/components/SendMessageForm/SendMessageForm.js
@@ -4,8 +4,9 @@ function SendMessageForm() {
 
     return (
         <>
-            <h2 className='form__title'>Отправить сообщение</h2>
+
             <form className="form">
+                <h2 className='form__title'>Отправить сообщение</h2>
                 <input className='form__input form__input_type_name' placeholder='Ваше имя' required></input>
                 <input className='form__input form__input_type_email' placeholder='Email' type='email'></input>
                 <textarea className='form__input form__input_type_message' placeholder='Ваше сообщение' required></textarea>
