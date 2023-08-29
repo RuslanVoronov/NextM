8799e22b682e20fef5e48725ba702b2844d64ae5
Ruslan Voronov <voronov.rv.99@yandex.ru>
2023-08-30 00:10:50 +0300

    Swiper

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,16 +1,19 @@
 import './Promo.css'
 import title from '../../images/title.svg'
+import vector from '../../images/Vector_white.svg'
 import message from '../../images/message.png'
 
 function Promo() {
 
     return (
         <main className="promo">
-            <img src={title} alt="Логотип" className="promo__title" />            
+            <img src={title} alt="Промо" className="promo__title" />
             <button className='promo__sign-up-button'>зарегистрироваться</button>
-            <img src={message} alt="Логотип" className="promo__message-image" />
-            <h2 className='promo__subtitle'>Команда рекламного холдинга GroupM рада открыть для вас двери digital-конференции NextM!</h2>   
-            <p className='promo__text'>NextM – это ежегодная digital-конференция, организованная ведущей международной рекламной группой GroupM, входящей в состав коммуникационного холдинга WPP и представленной в России агентствами Mindshare, MediaCom и Wavemaker.</p>
+            <img src={message} alt="Диалог" className="promo__message-image" />
+            <h2 className='promo__subtitle'>Команда рекламного холдинга GroupM рада открыть для вас двери digital-конференции NextM!</h2>
+            <p className='promo__text'>NextM – это ежегодная digital-конференция, организованная ведущей международной рекламной
+                группой GroupM, входящей в состав коммуникационного холдинга WPP и представленной в России агентствами Mindshare, MediaCom и Wavemaker.</p>
+            <img src={vector} alt="Вектор" className="promo__vector" />
         </main >
     );
 }
--- src/components/Spikers/Spikers.js
+++ src/components/Spikers/Spikers.js
@@ -1,13 +1,77 @@
+// Import Swiper React components
+import React from 'react';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// Import Swiper styles
+import 'swiper/css';
+
 import './Spikers.css'
 import spiker from '../../images/Spiker.png'
+import vector from '../../images/Vector_black.svg'
 
 function Spikers() {
+
 
     return (
         <section className="spikers">
             <h2 className='spikers__title'>Спикеры</h2>
-            <img src={spiker} alt="Спикер" className="spikers__image" />
-            <img src={spiker} alt="Спикер" className="spikers__image" />
+
+
+            <Swiper
+                grabCursor={true}
+                loop={true}
+                className='swiper'
+                modules={[Navigation, Pagination]}
+                spaceBetween={40}
+                slidesPerView={2}
+                navigation={{
+                    draggable: true,
+                    clickable: true,
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                }
+                }
+                pagination={{
+                    clickable: true,
+                    // dynamicBullets: true,
+                    // el: '.swiper-pagination', 
+                }}
+                style={{
+                    "--swiper-pagination-color": "#0A2756",
+                    "--swiper-pagination-bullet-inactive-color": "#E4E4E4",
+                    "--swiper-pagination-bullet-inactive-opacity": "1",
+                    "--swiper-pagination-bullet-size": "13px",
+                    "--swiper-pagination-bullet-horizontal-gap": "32px"
+                }}
+            >
+                <SwiperSlide>
+                    <img src={spiker} alt="Спикер" className="spiker" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={spiker} alt="Спикер" className="spiker" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={spiker} alt="Спикер" className="spiker" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={spiker} alt="Спикер" className="spiker" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={spiker} alt="Спикер" className="spiker" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={spiker} alt="Спикер" className="spiker" />
+                </SwiperSlide>
+                <img className='swiper-button swiper-button-next' src={vector} alt='Стрелка' />
+                <img className='swiper-button swiper-button-prev' src={vector} alt='Стрелка' />
+                <div className='swiper-pagination'></div>
+            </Swiper>
 
         </section >
     );
